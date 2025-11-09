6c29a56f65791b887205abfccb287c78be590d1d
JaidenAGrimminck <jaidng1@gmail.com>
2025-11-09 18:00:47 +0000

    First Commit

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+# source /opt/ros/${ROS_DISTRO}/setup.bash
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "-- Welcome to the ROS ${ROS_DISTRO} Docker container! --"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+exec "/bin/bash"
