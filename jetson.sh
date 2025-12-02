9408f37c4e05538ed277bcaf8f79f99eded203e5
JaidenAGrimminck <jaidng1@gmail.com>
2025-12-02 17:09:29 +0000

    fixes to spi

--- jetson.sh
+++ jetson.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-mv ../ros2_ws ../og_ros2_ws
+mv ros2_ws og_ros2_ws_backup_$(date +%Y%m%d_%H%M%S)
+
 git clone -b the-mini-jetson https://github.com/cornellev/mini-brain.git
-mv mini-brain ../ros2_ws
 
 echo "Setup complete. The new ROS2 workspace is ready."
