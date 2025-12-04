1140fe06338078550da6975ecaeb949d39b20f36
JaidenAGrimminck <jaidng1@gmail.com>
2025-12-04 22:09:39 +0000

    added spi tests

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,4 @@
 
 # start the main application
 echo "Starting the application..."
-docker run -it --rm --privileged -v /dev/input:/dev/input --user rosuser mini-brain /bin/bash
+docker run -it --rm --privileged -v /dev:/dev --user rosuser mini-brain /bin/bash
