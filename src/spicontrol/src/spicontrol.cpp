2a4c16f9463458e943414a8932b697fce114ec08
JaidenAGrimminck <jaidng1@gmail.com>
2025-12-06 20:02:27 +0000

    forgot i changed the port

--- src/spicontrol/src/spicontrol.cpp
+++ src/spicontrol/src/spicontrol.cpp
@@ -8,7 +8,7 @@
 // include spicomms.cpp
 #include "spicomms.cpp"
 
-const std::string dev = "/dev/spidev0.0";
+const std::string dev = "/dev/spidev0.1";
 uint32_t speedHz = 1'000'000;  // 1 MHz
 
 SpiDevice spi(dev, speedHz, SPI_MODE_0, 8);
